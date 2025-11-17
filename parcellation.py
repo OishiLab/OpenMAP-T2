d480f0cc5c2f2fd938a508ee6f30dddd17db5ffb
sorapoke <sorposoul2r@gmail.com>
2025-11-18 07:57:58 +0900

    fixed process shape

--- parcellation.py
+++ parcellation.py
@@ -86,7 +86,7 @@ def main():
 
     # Determine the device to use (CUDA, MPS, or CPU)
     if torch.cuda.is_available():
-        device = torch.device("cuda:6")
+        device = torch.device("cuda")
     elif torch.backends.mps.is_available():
         device = torch.device("mps")
     else:
